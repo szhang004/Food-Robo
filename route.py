417173f69f63aa61adbe8bfb2019401d3fcf2559
Sonia Zhang <skzhang@usc.edu>
2023-12-23 14:17:59 -0800

    intersection coords

--- route.py
+++ route.py
@@ -1,5 +1,15 @@
 import requests
-api_url = "http://router.project-osrm.org/route/v1/foot/34.0212327,-118.2866796;34.0219843,-118.2893943?overview=false"
+import json
+
+api_url = "http://router.project-osrm.org/route/v1/foot/-118.283752,34.0215692;-118.2900851,34.0196828?overview=false&alternatives=true&steps=true"
 response = requests.get(api_url)
 response = response.json()
-print(response['message'])
+
+i = 0 
+
+while (1):
+    try:
+        print(response["routes"][0]['legs'][0]['steps'][0]['intersections'][i]['location'])
+        i += 1
+    except: 
+        break
